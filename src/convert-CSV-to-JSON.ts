c352af0131f3aca9fe0de0e8c862f506a04eecfe
jpenno <jpenno123@hotmail.com>
2020-10-13 15:04:03 +1100

    add get values out of csv file

--- src/convert-CSV-to-JSON.ts
+++ src/convert-CSV-to-JSON.ts
@@ -1,12 +1,17 @@
 import fs from "fs";
+let endOfNames: number;
+let names: Array<string> = [];
+let values: Array<string> = [];
+
 const getValueNames = (file: string) => {
-  let names: Array<string> = [];
   let name = "";
+  let i = 0;
   const addName = () => {
     names.push(name);
     name = "";
   };
   for (const char of file) {
+    i++;
     if (char === "\r" || char === "\n") {
       addName();
       break;
@@ -15,13 +20,29 @@ const getValueNames = (file: string) => {
       name += char;
     } else addName();
   }
+  endOfNames = i + 1;
   return names;
+};
+
+const getValues = (file: string) => {
+  if (!endOfNames || names.length === 0) getValueNames(file);
+
+  let value = "";
+  let tmpfile = file.replace(/\r?\n|\r/g, ",");
+  for (let i = endOfNames - 1; i < tmpfile.length; i++) {
+    if (tmpfile[i] === ",") {
+      values.push(value);
+      value = "";
+    } else value += tmpfile[i];
+  }
+  values.push(value);
+  return values;
 };
 
 const readFile = (path: string) => {
   const file = fs.readFileSync(path).toString();
   const names = getValueNames(file);
-  console.log(names);
+  const values = getValues(file);
 };
 
-export { getValueNames, readFile };
+export { getValueNames, readFile, getValues };
--- test/convert-CSV-to-JSON.test.ts
+++ test/convert-CSV-to-JSON.test.ts
@@ -1,4 +1,4 @@
-import { getValueNames } from "../src/convert-CSV-to-JSON";
+import { getValueNames, getValues } from "../src/convert-CSV-to-JSON";
 import fs from "fs";
 const testFile1 = fs
   .readFileSync(__dirname + "/data/basic test data.csv")
@@ -7,5 +7,27 @@ const testFile1 = fs
 test("should show csv value names", () => {
   const results = getValueNames(testFile1);
   const answer = ["name 1", "name 2", "name 3", "name 4", "name 5"];
+  expect(results).toEqual(answer);
+});
+
+test("should show csv values", () => {
+  const results = getValues(testFile1);
+  const answer = [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "3",
+    "3",
+    "3",
+    "3",
+    "3",
+  ];
   expect(results).toEqual(answer);
 });
