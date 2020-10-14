5aa23c5d155a3cffccd4e3d6aa0772081c270a92
jpenno <jpenno123@hotmail.com>
2020-10-14 23:05:50 +1100

    add save as JSON file

--- src/convert-CSV-to-JSON.ts
+++ src/convert-CSV-to-JSON.ts
@@ -49,14 +49,34 @@ const getValues = (file: string) => {
       } else values.push(element);
     }
   }
-  console.log("values", values);
+  // console.log("values", values);
   return values;
+};
+
+const makeJSONString = (names: Array<string>, values: Array<string>) => {
+  let j = 0;
+  let outPut = "[\n";
+  for (let i = 0; i < values.length; i++, j++) {
+    if (j >= names.length) j = 0;
+    if (j === 0) outPut += "{\n";
+    const tmpVal = values[i].replace(/"/g, "");
+    outPut += `"${names[j]}": "${tmpVal}"`;
+    // j++;
+    if (i >= values.length - 1) break;
+    if (j >= names.length - 1) outPut += "\n},\n";
+    else outPut += ",\n";
+  }
+  outPut += "\n}\n]";
+  console.log(outPut);
+  return outPut;
 };
 
 const readFile = (path: string) => {
   const file = fs.readFileSync(path).toString();
   const names = getValueNames(file);
   const values = getValues(file);
+  const JSONFile = makeJSONString(names, values);
+  fs.writeFileSync("test.json", JSONFile);
 };
 
 export { getValueNames, readFile, getValues };
